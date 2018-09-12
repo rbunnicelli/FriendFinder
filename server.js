6d56a3646f8cacec5ee7d5c62692ba3c8f0c85a2
Machoman <rbunnicelli2010@yahoo.com>
2018-09-12 14:52:52 -0400

    port listener added

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ var path = require('path');
 var app = express();
 
 //sets initial port. used in listener
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 3000 ;
+
+//expose public directory to use the html docs
+//app.use(express.static(path.join(_dirname, './app/public')));//
 
 //sets up express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//routes
+//require(path.join("app/routing/apiRoutes"))(app);//
+//require(path.join("app/routing/htmlRoutes"))(app);//
+
+//listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT)
+})
+
