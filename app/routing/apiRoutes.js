32742b7629e489a2cfce9de9bcd45c44887666d3
Machoman <rbunnicelli2010@yahoo.com>
2018-09-13 15:48:15 -0400

    submit pushes to api friends

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,16 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
+    //Create new friends
+    app.post("/api/friends", function(req, res) {
+
+        var user = req.body;
+        console.log(user);
+
+        friends.push(user);
+        res.json(user);
+
+    });
 }
 
 
