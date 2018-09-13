c19770ed66550329f6f9a6df5589641813fedaf5
Machoman <rbunnicelli2010@yahoo.com>
2018-09-13 17:01:08 -0400

    results logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,10 +5,11 @@ var friends = require("../data/friend.js");
 
 //export api routes
 module.exports = function(app) {
-    //api get requests
+    //api get requests that shows all friends
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
+
     //Create new friends
     app.post("/api/friends", function(req, res) {
 
@@ -18,6 +19,29 @@ module.exports = function(app) {
         friends.push(user);
         res.json(user);
 
+        var scoresArray = [];
+        var newUserScore = req.body.scores;
+        var bestMatch = 0;
+    for(var i=0; i<friends.length; i++) {
+        var scoreDiff = 0
+
+        for(var j=0; j<user.length; j++) {
+            scoreDiff += (Math.abs(parseInt(friends[i].scores[j]) - parseInt(newUserScore[j])));
+        }
+
+        scoresArray.push(scoreDiff);
+    }
+
+    for(var i=0; i<scoresArray.length; i++) {
+        if(scoresArray[i] <= scoresArray[bestMatch]) {
+            bestMatch = i;
+        }
+    }
+
+    var bestFriendFound = friends[bestMatch];
+    res.json(bestFriendFound);
+
+    friends.push(req.body);
     });
 }
 
