8764b49f1685b3a3385b1e0865de7f7cbdb6d6eb
Machoman <rbunnicelli2010@yahoo.com>
2018-09-13 12:46:57 -0400

    modal functionality added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,10 +2,11 @@ var path = require('path');
 
 module.exports = function(app) {
     //html GET requests
+    //survey page
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
+    //default to home
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
